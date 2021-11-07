0b4772a8684c06bda1f856a9ec7925f5ed36e6d2
Deek3Arrows <deek@threearrowscommunication.com>
2021-11-06 19:15:53 -0500

    Nov5/6 Green Circles
    Basic page with a green circle. Figuring how to manipluation the js using PAPER.JS
    Playing around with my first for loop
    
    the js essentials book is where its at for me. downloaded GitHub Desktop so i can easily update this now.

---
+++ main.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict'
+paper.install(window);
+paper.setup(document.getElementById('mainCanvas'));
+//*SINGLE GREEN CIRCLE//
+/*var c = Shape.Circle(200, 200, 50);
+c.fillColor = 'green';
+c.strokeColor = 'black';*/
+paper.view.draw();
+//64 GREEN CIRCLES//
+  /*var c;
+  for(var x=25; x<400; x+=50) {
+    for(var y=25; y<400; y+=50) {
+      c = Shape.Circle(x, y, 20);
+      c.fillColor = 'green';
+    }
+  }*/
+//USER INPUT TOOL//
+var tool = new Tool();
+tool.onMouseDown = function(event) {
+  var c = Shape.Circle(event.point.x, event.point.y, 20);
+  c.fillColor = 'green';
+};
+  ('main.js loaded')
+}())
